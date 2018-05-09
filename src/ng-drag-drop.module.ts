68a3a76610fe8fe356d82b4867642504f3003213
Flemming Bregnvig <flemming@bregnvig.dk>
2018-05-09 13:28:29 +0200

    Fix declaration and export

--- src/ng-drag-drop.module.ts
+++ src/ng-drag-drop.module.ts
@@ -1,21 +1,25 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { Draggable } from './directives/draggable.directive';
-import { Droppable } from './directives/droppable.directive';
-import { NgDragDropService } from './services/ng-drag-drop.service';
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {Draggable} from './directives/draggable.directive';
+import {Droppable} from './directives/droppable.directive';
+import {NgDragDropService} from './services/ng-drag-drop.service';
 
 
 @NgModule({
-  imports: [],
-  declarations: [
-  ],
-  exports: [
-  ]
+    imports: [],
+    declarations: [
+        Draggable,
+        Droppable
+    ],
+    exports: [
+        Draggable,
+        Droppable
+    ]
 })
 export class NgDragDropModule {
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: NgDragDropModule,
-      providers: [NgDragDropService]
-    };
-  }
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: NgDragDropModule,
+            providers: [NgDragDropService]
+        };
+    }
 }
